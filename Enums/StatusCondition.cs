43ed2159f48552d231bff47c750e80d5b15fe293
superalexander64 <alexandersteenandersen@hotmail.com>
2025-08-09 21:32:01 +0200

    add binary comments to status condition

--- Enums/StatusCondition.cs
+++ Enums/StatusCondition.cs
@@ -2,11 +2,11 @@ namespace PokemonSaveFileEditor.Enums
 {
     public enum StatusCondition : int
     {
-        OK = 0,
-        ASLEEP = 4,
-        POISONED = 8,
-        BURNED = 16,
-        FROZEN = 32,
-        Paralyzed = 64
+        OK        = 0,  // Binary 00000000 
+        ASLEEP    = 4,  // Binary 00000100 
+        POISONED  = 8,  // Binary 00001000 
+        BURNED    = 16, // Binary 00010000 
+        FROZEN    = 32, // Binary 00100000 
+        Paralyzed = 64  // Binary 01000000 
     }
 }
